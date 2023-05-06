911944f012e0aa435445cfa62b99ee7b22f923ec
siawashkasra <siawashkasra@gmail.com>
2023-05-05 22:05:49 -0600

    Add basic test setup

---
+++ test_web.py
@@ -0,0 +1,28 @@
+
+import pytest
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait as wait
+from selenium.webdriver.support import expected_conditions as EC
+url = "https://www.mozilla.org/en-US/"
+title = "Internet for people"
+
+
+@pytest.fixture
+def webFix():
+    driver = webdriver.Firefox()
+    driver.get(url)
+    try:
+        element = wait(driver, 10).until(EC.title_contains(title))
+    except Exception as e:
+        print(e)
+    yield driver
+
+    driver.quit()
+
+
+def test_web_link(webFix):
+    webFix.find_element(By.LINK_TEXT, 'Learn more').click()
+    title = webFix.title
+    assert "Firefox" in title
